c04abfb78061a779648d4e590ab8823a562e8387
x-VEspit <vincent.espitalier@cirad.fr>
2024-05-04 21:45:06 +0200

    * Algorithmes de tri dans un fichier à part * Implémentation tri fusion simplifée (en plus de la version optimisée) * Autres améliorations mineuses (noms de variables etc.

---
+++ src/algos_tri.rs
@@ -0,0 +1,327 @@
+
+// Algorithme du tri par sélection
+// Entrée = Sortie:
+// ma_liste: liste d'entiers 'ma_liste'
+// La liste est triée en place
+// Complexité: n^2
+// Plus précisément: (n(n-1)/2 comparaisons exactement, (n-1) permutations maximum)
+// Voir: https://fr.wikipedia.org/wiki/Tri_par_s%C3%A9lection
+pub fn tri_par_selection(ma_liste: &mut [i32])
+{
+    println!("tri_par_selection > appel");
+
+    let n = ma_liste.len();
+
+    // Cas particulier d'une liste réduite à un seul élément.
+    if n <= 1 {return;}
+
+    // On trie les élements du tableau, successivement
+    // for i in range(0, n):
+    for i in 0..n
+    {
+        let mut m = ma_liste[i];
+        let mut m_index = i;
+
+        // Recherche du plus petit élément, parmi les éléments non triés
+        for j in (i+1)..n
+        {
+            let v = ma_liste[j];
+            if v < m {m = v; m_index = j;}
+
+        }
+
+        // Le i-eme élement le plus petit du tableau se trouve en position m_index, et vaut m
+        // On permute les élement d'index i et m_index
+        if (i != m_index)
+        {
+            //println!("Echange les index {} et {}", i, m_index);
+            let v_swap = ma_liste[i];
+            ma_liste[i] = m;
+            ma_liste[m_index] = v_swap;
+        }
+    }
+
+    // 
+    // return ma_liste
+} // fn tri_par_selection ()
+
+
+// Algorithme du tri rapide
+// Entrée = Sortie:
+// ma_liste: liste d'entiers 'ma_liste'
+// Implémentation tri en place, non stable
+// Complexité: n.log(n)
+// Version standard, non optimisée: Allocation interne à chaque appel pour construire les sous-listes
+// Voir: https://fr.wikipedia.org/wiki/Tri_rapide
+pub fn tri_rapide(ma_liste: &mut [i32])
+{
+    let n = ma_liste.len();
+
+    // Gestion des cas particuliers (fin des appels récursifs)
+    // Liste avec 1 seul élément (-> Plus rien à trier)
+    if n <= 1 {return;}
+
+    // Cas général, menant à 2 appels résursifs pour trier les 2 sous-listes
+
+    let pivot = ma_liste[0];  // Existe car la liste a au moins 2 éléments
+
+    let mut ma_liste_gauche_vec: Vec<i32> = Vec::new();
+    let mut ma_liste_droite_vec: Vec<i32> = Vec::new();
+
+    for index in 1..n
+    {
+        let valeur = ma_liste[index];
+        if (valeur <= pivot) {ma_liste_gauche_vec.push(valeur);}
+        if (valeur > pivot) {ma_liste_droite_vec.push(valeur);}
+
+    }
+
+    let ma_liste_gauche: &mut [i32] = ma_liste_gauche_vec.as_mut_slice();
+    let ma_liste_droite: &mut [i32] = ma_liste_droite_vec.as_mut_slice();
+    tri_rapide(ma_liste_gauche);
+    tri_rapide(ma_liste_droite);
+
+    // Rassemble les sous-listes triées en les copiant dans le tableau d'origine
+    let mut cpt:usize = 0;
+    for index in 0..ma_liste_gauche.len()
+    {
+        ma_liste[cpt] = ma_liste_gauche[index];
+        cpt += 1;
+    }
+
+    ma_liste[cpt] = pivot;
+    cpt += 1;
+
+    for index in 0..ma_liste_droite.len()
+    {
+        ma_liste[cpt] = ma_liste_droite[index];
+        cpt += 1;
+    }
+
+}
+
+
+// Algorithme du tri fusion
+// Entrée = Sortie:
+// ma_liste: liste d'entiers 'ma_liste'
+// Implémentation tri stable et en place
+// Version standard, non optimisée: Allocation interne à chaque appel pour fusionner les sous-listes
+// Complexité: n.log(n)
+// Voir: https://fr.wikipedia.org/wiki/Tri_fusion
+pub fn tri_fusion(ma_liste: &mut [i32])
+{
+    let n = ma_liste.len();
+
+    // Gestion des cas particuliers (fin des appels récursifs)
+    // Liste avec 1 seul élément (-> Plus rien à trier)
+    if n <= 1 {return;}
+
+    // Gestion des cas particuliers (fin des appels récursifs)
+    // Liste avec 2 élements
+    if n == 2
+    {
+        // Permuter les elements d'indice 0 et 1 si nécessaire
+        if ma_liste[0] > ma_liste[1]
+        {
+            let v_swap = ma_liste[1];
+            ma_liste[1] = ma_liste[0];
+            ma_liste[0] = v_swap;
+        }
+        // Algorithme 'en place': On modifie directement le tableau en entrée,
+        // donc pas de valeur de retour
+        return;
+    }
+
+    // Cas général, menant à 2 appels récursifs, suivis de la fusion des 2 sous-listes
+    let mid = n / 2;
+
+    // On extrait les 2 sous-listes: Moitiés gauche et droite du tableau
+    // https://doc.rust-lang.org/std/primitive.slice.html#method.split_at_mut
+    let (ma_sous_liste_gauche, ma_sous_liste_droite) = ma_liste.split_at_mut(mid);
+
+
+    // Appels récursifs pour trier chacune des 2 sous-listes
+    tri_fusion(ma_sous_liste_gauche);
+    tri_fusion(ma_sous_liste_droite);
+
+
+    // Fusion des 2 sous-listes
+    let mut ma_liste_bis_vec: Vec<i32> = Vec::new();
+
+    let mut ma_liste_gauche_index: usize = 0;
+    let mut ma_liste_droite_index: usize = 0;
+
+    // Fusion (itérative) des 2 listes
+    // On boucle sur le tableau principal, que l'on remplit.
+    //
+    // A chaque itération, on prend le plus petit élément des 2 listes
+    // et on le met dans le tableau principal
+    // 
+    // Invariant de boucle: A la fin de chaque itération, le tableau principal
+    // contient les 'index' élements les plus petits des 2 sous-listes.
+    for index in 0..n
+    {
+        if (ma_liste_gauche_index < ma_sous_liste_gauche.len()) &&
+        (ma_liste_droite_index < ma_sous_liste_droite.len())
+        {
+            // Cas général ou les 2 sous-listes contiennent encore des élements à traiter
+            let v_gauche = ma_sous_liste_gauche[ma_liste_gauche_index];
+            let v_droite = ma_sous_liste_droite[ma_liste_droite_index];
+            if v_gauche <= v_droite
+            {
+                ma_liste_bis_vec.push(v_gauche);
+                ma_liste_gauche_index += 1;
+            }
+            else
+            {
+                ma_liste_bis_vec.push(v_droite);
+                ma_liste_droite_index += 1;
+            }
+        }
+        else if (ma_liste_gauche_index < ma_sous_liste_gauche.len())
+        {
+            // La liste droite a été entièrement traitée
+            // On prend de la liste gauche
+            let v_gauche = ma_sous_liste_gauche[ma_liste_gauche_index];
+            ma_liste_bis_vec.push(v_gauche);
+            ma_liste_gauche_index += 1;
+        }
+        else if (ma_liste_droite_index < ma_sous_liste_droite.len())
+        {
+            // La liste gauche a été entièrement traitée
+            // On prend de la liste droite
+            let v_droite = ma_sous_liste_droite[ma_liste_droite_index];
+            ma_liste_bis_vec.push(v_droite);
+            ma_liste_droite_index += 1;
+        }
+        else
+        {
+            // Ce cas ne devrait jamais se produire
+            println!("Erreur interne: La liste principale n'est pas remplie, mais les 2 sous-listes ont été entèrement traitées.")
+        }
+
+    }  // for index in 0..n
+
+    let ma_liste_bis: &mut [i32] = ma_liste_bis_vec.as_mut_slice();
+    ma_liste.clone_from_slice(&ma_liste_bis);
+    return;
+
+} // fn tri_fusion
+
+
+
+
+// Algorithme du tri fusion - implémentation améliorée (une seule allocation mémoire supplémentaire)
+// Entrée = Sortie:
+// ma_liste: liste d'entiers 'ma_liste'
+// Implémentation tri stable et en place
+// Complexité: n.log(n)
+// Version optimisée: Une seule allocation interne au premier appel à la fonction, de taille moitié du tableau d'origine
+// Voir: https://fr.wikipedia.org/wiki/Tri_fusion
+pub fn tri_fusion_ameliore(ma_liste: &mut [i32], index_min_opt : Option<usize>, index_max_opt : Option<usize>, ma_sous_liste_1_opt: Option<&mut [i32]>)
+{
+    let arguments_manquants: bool = (index_min_opt == None) || (index_max_opt == None) 
+                                        || ((ma_sous_liste_1_opt.as_ref()).is_none());
+    if arguments_manquants
+    {
+        println!("Appel à tri_fusion_ameliore");
+        // On traite le tout premier appel à la fonction, effectué par l'utilisateur (i.e. non récursif)
+        let len_liste = ma_liste.len();
+        let len_sous_liste_1: usize  = len_liste / 2 + 1;
+        // Unique allocation de cet algorithme - Effectué une seule fois lors de l'appel utilisateur
+        let mut ma_sous_liste_1_vec: Vec<i32> = vec![0 as i32; len_sous_liste_1];
+        let ma_sous_liste_1: &mut [i32] = &mut ma_sous_liste_1_vec.as_mut_slice();
+        // Relance la fonction, avec le tableau alloué, et les bons index cette fois
+        return tri_fusion_ameliore(ma_liste, Some(0), Some(len_liste - 1), Some(ma_sous_liste_1));
+    }
+
+    // A partir d'ici, on sait que les arguments optionnels sont effectivement fournis (pas None)
+    // index_min_opt, index_max_opt et ma_sous_liste_1_opt
+    // On peut unwrap() sans risque
+    let index_min: usize = index_min_opt.unwrap();
+    let index_max: usize = index_max_opt.unwrap();
+    let ma_sous_liste_1: &mut [i32] = ma_sous_liste_1_opt.unwrap();
+
+    let n = index_max - index_min + 1;
+
+    // Gestion des cas particuliers (fin des appels récursifs)
+    // Liste avec 1 seul élément (-> Plus rien à trier)
+    if n <= 1 {return;}
+
+    // Gestion des cas particuliers (fin des appels récursifs)
+    // Liste avec 2 élements
+    if n == 2
+    {
+        // Permuter les elements d'indice 0 et 1 si nécessaire
+        if ma_liste[index_min] > ma_liste[index_max]
+        {
+            let v_swap = ma_liste[index_max];
+            ma_liste[index_max] = ma_liste[index_min];
+            ma_liste[index_min] = v_swap;
+        }
+        return;
+    }
+
+    // Cas général, menant à 2 appels récursifs, suivis de la fusion des 2 sous-listes
+    let mid = (index_min + index_max) / 2;
+    let mid_plus_1 = mid + 1;
+
+    // Appels récursifs pour trier chacune des 2 sous-listes
+    tri_fusion_ameliore(ma_liste, Some(index_min), Some(mid), Some(ma_sous_liste_1));
+    tri_fusion_ameliore(ma_liste, Some(mid_plus_1), Some(index_max), Some(ma_sous_liste_1));
+
+
+    // Fusion des 2 sous-listes
+    // On copie la premiere sous-liste dans un tableau à part
+    // puis on effectue la fusion dans le tableau principal
+    let len_sous_liste_1 = mid - index_min + 1;
+
+    ma_sous_liste_1[..len_sous_liste_1].copy_from_slice(&mut ma_liste[index_min..(mid + 1)]);
+
+    let mut fusion_index_input_1 = 0;
+    let mut fusion_index_input_2 = mid_plus_1;
+
+    for fusion_index_output in index_min..(index_max + 1)
+    {
+        let liste_1_non_epuisee = (fusion_index_input_1 <= (mid - index_min));
+        let liste_2_non_epuisee = (fusion_index_input_2 <= index_max);
+
+        if (liste_1_non_epuisee)
+        {
+            if (liste_2_non_epuisee)
+            {
+                if (ma_liste[fusion_index_input_2] < ma_sous_liste_1[fusion_index_input_1])
+                {
+                    // On copie un élément de la liste 2
+                    ma_liste[fusion_index_output] = ma_liste[fusion_index_input_2];
+                    fusion_index_input_2 += 1;
+                }
+                else
+                {
+                    // En cas d'égalité, on prend en priorité un élement de la première liste
+                    // => Tri stable
+
+                    // On copie un élément de la liste 1
+                    ma_liste[fusion_index_output] = ma_sous_liste_1[fusion_index_input_1];
+                    fusion_index_input_1 += 1;
+                }
+            }
+            else {
+                // Liste 2 épuisée
+                // On copie un élément de la liste 1
+                ma_liste[fusion_index_output] = ma_sous_liste_1[fusion_index_input_1];
+                fusion_index_input_1 += 1;
+            
+            }
+        }
+        else if (liste_2_non_epuisee)
+        {
+            // Liste 1 épuisée
+            // On copie un élément de la liste 2
+            ma_liste[fusion_index_output] = ma_liste[fusion_index_input_2];
+            fusion_index_input_2 += 1;
+        }
+
+    }
+
+} // fn tri_fusion_ameliore
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 
+// Les implémentations des algorithmes de tri sont dans le fichier algos_tri.rs
+// On inclut ce 'module'
+mod algos_tri;
+
+
 // Implémentation récursive de la factorielle
 // Entrée: n entier
 // Sortie: n! = n * (n - 1) * (n - 2) * ... * 1
@@ -186,235 +191,6 @@ fn recherche_dichotomique(ma_liste: &mut [i32], val_recherche: i32, index_min_op
 
 
 
-// Algorithme du tri par sélection
-// Entrée = Sortie:
-// ma_liste: liste d'entiers 'ma_liste'
-// La liste est triée en place
-// Complexité: n^2
-// Plus précisément: (n(n-1)/2 comparaisons exactement, (n-1) permutations maximum)
-// Voir: https://fr.wikipedia.org/wiki/Tri_par_s%C3%A9lection
-fn tri_par_selection(ma_liste: &mut [i32])
-{
-    println!("tri_par_selection > appel");
-
-    let n = ma_liste.len();
-
-    // Cas particulier d'une liste réduite à un seul élément.
-    if n <= 1 {return;}
-
-    // On trie les élements du tableau, successivement
-    // for i in range(0, n):
-    for i in 0..n
-    {
-        let mut m = ma_liste[i];
-        let mut m_index = i;
-
-        // Recherche du plus petit élément, parmi les éléments non triés
-        for j in (i+1)..n
-        {
-            let v = ma_liste[j];
-            if v < m {m = v; m_index = j;}
-
-        }
-
-        // Le i-eme élement le plus petit du tableau se trouve en position m_index, et vaut m
-        // On permute les élement d'index i et m_index
-        if (i != m_index)
-        {
-            //println!("Echange les index {} et {}", i, m_index);
-            let v_swap = ma_liste[i];
-            ma_liste[i] = m;
-            ma_liste[m_index] = v_swap;
-        }
-    }
-
-    // 
-    // return ma_liste
-} // fn tri_par_selection ()
-
-
-// Algorithme du tri rapide
-// Entrée = Sortie:
-// ma_liste: liste d'entiers 'ma_liste'
-// Implémentation tri en place, non stable
-// Complexité: n.log(n)
-// Version standard, non optimisée: Allocations internes pour construire les sous-listes
-// Voir: https://fr.wikipedia.org/wiki/Tri_rapide
-fn tri_rapide(ma_liste: &mut [i32])
-{
-    let n = ma_liste.len();
-
-    // Gestion des cas particuliers (fin des appels récursifs)
-    // Liste avec 1 seul élément (-> Plus rien à trier)
-    if n <= 1 {return;}
-
-    // Cas général, menant à 2 appels résursifs pour trier les 2 sous-listes
-
-    let pivot = ma_liste[0];  // Existe car la liste a au moins 2 éléments
-
-    let mut ma_liste_gauche: Vec<i32> = Vec::new();
-    let mut ma_liste_droite: Vec<i32> = Vec::new();
-
-    for index in 1..n
-    {
-        let valeur = ma_liste[index];
-        if (valeur <= pivot) {ma_liste_gauche.push(valeur);}
-        if (valeur > pivot) {ma_liste_droite.push(valeur);}
-
-    }
-
-    let ma_liste_gauche_slice: &mut [i32] = ma_liste_gauche.as_mut_slice();
-    let ma_liste_droite_slice: &mut [i32] = ma_liste_droite.as_mut_slice();
-    tri_rapide(ma_liste_gauche_slice);
-    tri_rapide(ma_liste_droite_slice);
-
-    // Rassemble les sous-listes triées en les copiant dans le tableau d'origine
-    let mut cpt:usize = 0;
-    for index in 0..ma_liste_gauche_slice.len()
-    {
-        ma_liste[cpt] = ma_liste_gauche_slice[index];
-        cpt += 1;
-    }
-
-    ma_liste[cpt] = pivot;
-    cpt += 1;
-
-    for index in 0..ma_liste_droite_slice.len()
-    {
-        ma_liste[cpt] = ma_liste_droite_slice[index];
-        cpt += 1;
-    }
-
-}
-
-
-// Algorithme du tri fusion
-// Entrée = Sortie:
-// ma_liste: liste d'entiers 'ma_liste'
-// Implémentation tri stable et en place
-// Complexité: n.log(n)
-// Alloue un tableau de taille moitié (une seule fois) pour stocker les valeurs intermédiaires
-// et permettre la stabilité du tri
-// Voir: https://fr.wikipedia.org/wiki/Tri_fusion
-fn tri_fusion(ma_liste: &mut [i32], index_min_opt : Option<usize>, index_max_opt : Option<usize>, ma_sous_liste_1_opt: Option<&mut [i32]>)
-{
-    if ((index_min_opt != None) && (index_max_opt != None))
-    {
-        //println!("Appel à tri_fusion: {} {}", index_min_opt.unwrap(), index_max_opt.unwrap());
-    }
-    else {
-        println!("Appel à tri_fusion");
-    }
-
-    let arguments_manquants: bool = (index_min_opt == None) || (index_max_opt == None) || ((ma_sous_liste_1_opt.as_ref()).is_none());
-    match (arguments_manquants)
-    {
-        true => {
-            let len_liste = ma_liste.len();
-            let len_sous_liste_1: usize  = len_liste / 2 + 1;
-            // Allocation sous tableau
-            let mut ma_sous_liste_1_vec: Vec<i32> = vec![0 as i32; len_sous_liste_1];
-            let ma_sous_liste_1: &mut [i32] = &mut ma_sous_liste_1_vec.as_mut_slice();
-            return tri_fusion(ma_liste, Some(0), Some(len_liste - 1), Some(ma_sous_liste_1));
-        }
-        false => {
-
-            // A partir d'ici, on sait que les arguments optionnels sont effectivement fournis (pas None)
-            // index_min_opt, index_max_opt et ma_sous_liste_1_opt
-            // On peut unwrap() sans risque
-            let index_min: usize = index_min_opt.unwrap();
-            let index_max: usize = index_max_opt.unwrap();
-            let ma_sous_liste_1: &mut [i32] = ma_sous_liste_1_opt.unwrap();
-
-            let n = index_max - index_min + 1;
-
-            // Gestion des cas particuliers (fin des appels récursifs)
-            // Liste avec 1 seul élément (-> Plus rien à trier)
-            if n <= 1 {return;}
-
-            // Gestion des cas particuliers (fin des appels récursifs)
-            // Liste avec 2 élements
-            if n == 2
-            {
-                // Permuter les elements d'indice 0 et 1 si nécessaire
-                if ma_liste[index_min] > ma_liste[index_max]
-                {
-                    let v_swap = ma_liste[index_max];
-                    ma_liste[index_max] = ma_liste[index_min];
-                    ma_liste[index_min] = v_swap;
-                }
-                return;
-            }
-
-            // Cas général, menant à 2 appels récursifs, suivis de la fusion des 2 sous-listes
-            let mid = (index_min + index_max) / 2;
-            let mid_plus_1 = mid + 1;
-
-            // Appels récursifs pour trier chacune des 2 sous-listes
-            tri_fusion(ma_liste, Some(index_min), Some(mid), Some(ma_sous_liste_1));
-            tri_fusion(ma_liste, Some(mid_plus_1), Some(index_max), Some(ma_sous_liste_1));
-
-
-            // Fusion des 2 sous-listes
-            // On copie la premiere sous-liste (la plus "grosse") dans un tableau à part
-            // puis on effectue la fusion dans le tableau principal
-            let len_sous_liste_1 = mid - index_min + 1;
-
-            ma_sous_liste_1[..len_sous_liste_1].copy_from_slice(&mut ma_liste[index_min..(mid + 1)]);
-
-            let mut fusion_index_input_1 = 0;
-            let mut fusion_index_input_2 = mid_plus_1;
-
-            for fusion_index_output in index_min..(index_max + 1)
-            {
-                let liste_1_non_epuisee = (fusion_index_input_1 <= (mid - index_min));
-                let liste_2_non_epuisee = (fusion_index_input_2 <= index_max);
-
-                if (liste_1_non_epuisee)
-                {
-                    if (liste_2_non_epuisee)
-                    {
-                        if (ma_liste[fusion_index_input_2] < ma_sous_liste_1[fusion_index_input_1])
-                        {
-                            // On copie un élément de la liste 2
-                            ma_liste[fusion_index_output] = ma_liste[fusion_index_input_2];
-                            fusion_index_input_2 += 1;
-                        }
-                        else
-                        {
-                            // En cas d'égalité, on prend en priorité un élement de la première liste
-                            // => Tri stable
-
-                            // On copie un élément de la liste 1
-                            ma_liste[fusion_index_output] = ma_sous_liste_1[fusion_index_input_1];
-                            fusion_index_input_1 += 1;
-                        }
-                    }
-                    else {
-                        // Liste 2 épuisée
-                        // On copie un élément de la liste 1
-                        ma_liste[fusion_index_output] = ma_sous_liste_1[fusion_index_input_1];
-                        fusion_index_input_1 += 1;
-                    
-                    }
-                }
-                else if (liste_2_non_epuisee)
-                {
-                    // Liste 1 épuisée
-                    // On copie un élément de la liste 2
-                    ma_liste[fusion_index_output] = ma_liste[fusion_index_input_2];
-                    fusion_index_input_2 += 1;
-                }
-
-
-            }
-            
-        }
-    }
-
-} // fn tri_fusion
-
-
 // Fonction vérifiant qu'une liste est croissante
 // Entrée = ma_liste: liste d'entiers
 // Sortie = un booleen. true -> liste croissante..
@@ -575,9 +351,11 @@ fn main() {
         println!("\nRecherche lineaire de la valeur {}: index {} \n", p, recherche_lineaire(ma_liste2, p).unwrap());
         //println!("Recherche dichotomique de la valeur {}: index {}", p, recherche_dichotomique(ma_liste2, p, None, None).unwrap());
 
-        //tri_par_selection(ma_liste2);
-        //tri_fusion(ma_liste2, None, None, None);
-        tri_rapide(ma_liste2);
+        //algos_tri::tri_par_selection(ma_liste2);
+        
+        //algos_tri::tri_rapide(ma_liste2);
+        algos_tri::tri_fusion(ma_liste2);
+        //algos_tri::tri_fusion_ameliore(ma_liste2, None, None, None);
 
         println!("Liste triée: \n{:?}", &ma_liste2);
         assert!(verif_liste_croissante(&ma_liste2), "Erreur: la liste n'est pas correctement triée.");
